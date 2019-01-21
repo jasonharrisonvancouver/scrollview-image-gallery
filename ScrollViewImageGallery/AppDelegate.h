f16c8a44816b837219df3b1a586e5618f9847944
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-21 14:33:02 -0800

    Initial Commit

---
+++ ScrollViewImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGallery
+//
+//  Created by jason harrison on 2019-01-21.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
